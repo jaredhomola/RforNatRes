a2bd73995eec764140a80ae7a38bfc5ed94be01f
Jared Homola <40047588+jaredhomola@users.noreply.github.com>
2022-02-06 19:26:53 -0600

    Add files via upload

---
+++ InClass_Wk3_Mon.R
@@ -0,0 +1,13 @@
+#########################################
+### In class exercises - Feb 7, 2022 ####
+#########################################
+
+library(tidyverse)
+
+tib <- tibble(day = c("1", "2", "3"), 
+              timOfDay = c("day", "day", "night"), 
+              deer = c("yes", "yes", "no"),
+              turkey = c("yes", "no", NA),
+              bat = c(NA, NA, "no"))
+
+
