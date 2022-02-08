afc7d450e8c59c3f22e05155a10c9a59ccaf4c70
Jared Homola <40047588+jaredhomola@users.noreply.github.com>
2022-02-08 09:37:40 -0600

    Update InClass_Wk3_Mon.R

--- InClass_Wk3_Mon.R
+++ InClass_Wk3_Mon.R
@@ -5,7 +5,7 @@
 library(tidyverse)
 
 tib <- tibble(day = c("1", "2", "3"), 
-              timOfDay = c("day", "day", "night"), 
+              timeOfDay = c("day", "day", "night"), 
               deer = c("yes", "yes", "no"),
               turkey = c("yes", "no", NA),
               bat = c(NA, NA, "no"))
