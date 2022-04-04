2f1eedeb9a889f63f3c4161eff5905b20c59e5cd
Jared Homola <40047588+jaredhomola@users.noreply.github.com>
2022-04-03 21:50:56 -0500

    Add files via upload

---
+++ Wk10_inClass_Mon.R
@@ -0,0 +1,63 @@
+library(lterdatasampler)
+library(tidyverse)
+
+##### Exercise 1 ######
+## Data set: lterdatasampler::ntl_icecover
+## Create a scatterplot for ice duration through time
+## Add custom x and y labels
+## Add a loess smoothed trend line with a "span" value of 1.5
+## Clean it up using a theme of your choosing
+
+
+
+
+#### Symbols ####
+
+
+
+#### Colors ####
+
+
+library(paletteer)
+
+  
+#### Legends ####
+
+
+
+#### Arrange multiple plots ####
+
+
+
+#### Annotate plots ####
+
+
+
+#### ggrepel ####
+
+
+##### Exercise 2 ######
+## Data set: lterdatasampler::and_vertebrates
+## Filter to just cutthroat trout
+## Create a density plot of weight_g
+## Add custom x and y labels
+## Clean it up using a theme of your choosing
+
+#### rug plots ####
+
+
+
+#### 2D density plots ####
+
+
+
+#### Bubble plot ####
+
+
+
+#### Before / after polot ####
+
+
+
+#### Pie charts ####
+
