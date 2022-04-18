4277c27bfa8f08d8008c2e970517b31ad386f198
Jared Homola <40047588+jaredhomola@users.noreply.github.com>
2022-04-18 08:03:03 -0500

    Add files via upload

---
+++ Wk11_inClassExercises_pt2.R
@@ -0,0 +1,42 @@
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(ggspatial)
+library(ggrepel)
+library(cowplot)
+library(tidyverse)
+
+
+##### Map 1: Great Lakes round goby ######
+### Data at https://raw.githubusercontent.com/jaredhomola/RforNatRes/gh-pages/gobyData.txt
+
+
+
+##### Map 2: Southern Green Bay with bathymetry raster ######
+library(raster)
+library(spatstat)
+
+#### Origin of raster: https://www.ncei.noaa.gov/maps/grid-extract/ ####
+### Raster data at: https://github.com/jaredhomola/RforNatRes/blob/gh-pages/southernGbBathymetry.tif?raw=true
+southGB <- raster()
+
+### Alternative Wisconsin outline at: https://data-wi-dnr.opendata.arcgis.com/
+
+coords <- tibble(
+  site = c("A", "B", "C", "D"),
+  long = c(-87.752, -87.954, -87.887, -87.989),
+  lat = c(44.73, 44.67, 44.61, 44.56)) %>% 
+  st_as_sf(., 
+           coords = c("long", "lat"), 
+           crs = 4326)
+
+
+
+###### Map 3: Google map of Schmeeckle Reserve #####
+### Google API key registration instruction: https://www.littlemissdata.com/blog/maps ###
+library(ggmap)
+library(ggsn)
+library(rphylopic)
+
+
+
