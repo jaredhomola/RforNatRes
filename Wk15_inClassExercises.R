6a7e4f87ce5ac55aa9ae3bd5f899ef800caac561
Jared Homola <40047588+jaredhomola@users.noreply.github.com>
2022-05-04 08:49:35 -0500

    Add files via upload

---
+++ Wk15_inClassExercises.R
@@ -0,0 +1,33 @@
+library(lterdatasampler)
+library(broom)
+library(tidyverse)
+
+### Principal component analysis ###
+## Run the analysis
+
+
+## Plot it
+
+
+## PCA scree plot
+
+  
+
+#### K-means clustering ####
+#install.packages("factoextra")
+library(factoextra)
+
+## Generate input data (no NAs)
+
+
+## Set up the K-means input data
+
+
+## How many clusters (K)?
+
+
+## Conduct K-means clustering analysis
+
+
+## Plot it
+
