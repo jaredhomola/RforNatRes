1ef4f1e548844554a51d319a69a0fcc1e60fc2d9
Jared Homola <40047588+jaredhomola@users.noreply.github.com>
2022-05-08 21:49:21 -0500

    Add files via upload

--- Wk15_inClassExercises.R
+++ Wk15_inClassExercises.R
@@ -1,24 +1,36 @@
 library(lterdatasampler)
 library(broom)
+library(ggrepel)
 library(tidyverse)
 
 ### Principal component analysis ###
+## Analysis goal: Summarize variation in tree measurements
+
+## Look at the data
+lterdatasampler::hbr_maples 
+
 ## Run the analysis
 
 
 ## Plot it
 
-
+  
 ## PCA scree plot
 
-  
+## Practice PCA
+ruralUrb <- read_csv("https://raw.githubusercontent.com/jaredhomola/WFUrbanAdaptation/master/extData/envdata.csv")
 
+
+
+  
 #### K-means clustering ####
+## Goal: Identify clusters of similar plant measurements
 #install.packages("factoextra")
 library(factoextra)
 
 ## Generate input data (no NAs)
-
+mapleDat <- lterdatasampler::hbr_maples %>% 
+  drop_na() 
 
 ## Set up the K-means input data
 
@@ -30,4 +42,47 @@ library(factoextra)
 
 
 ## Plot it
+library(ggpubr)
+
+
+
+
+#### NMDS ####
+## Analysis goal: Determine whether bacteria communities vary across Lake Michigan tributaries
+## and identify correlates of that variation.
+
+## Community data
+bact.dat <- read_table("https://raw.githubusercontent.com/ScribnerLab/LakeMichTribBacteria/main/extData/3.%20Bacterial_OTU_Matrix.txt") %>% 
+  separate(Group,
+           into = c("river", NA)) %>% 
+  distinct(river,
+           .keep_all = TRUE) %>% 
+  select(1:201) %>% 
+  filter(river != 'PereMarquette')
+
+## Environmental data
+river.dat <- read_csv("https://raw.githubusercontent.com/jaredhomola/RforNatRes/gh-pages/riverVars.csv") %>% 
+  select(-COMID) %>% 
+  filter(River %in% bact.dat$river) %>% 
+  mutate(side = c(rep("west", 5), rep("north", 2), 
+                  rep("west", 1), rep("east", 7), rep("north", 2)) %>% 
+           as_factor) %>% 
+  arrange(match(River, bact.dat$river)) 
+
+## Transform community data into relative abundances
+
+
+## Perform NMDS
+
+
+## Add in environmental vectors
+
+
+## Get tibble of NMDS coordinates
+
+
+## Get information for environmental vectors
+
+
+## Plot NMDS data using ggplot
 
