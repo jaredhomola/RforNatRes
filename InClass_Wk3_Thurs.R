eb739a46868d929fabdd8bf3b9c0ee60b324a391
Jared Homola <40047588+jaredhomola@users.noreply.github.com>
2024-02-08 09:40:22 -0600

    Updated week 3 materials

---
+++ InClass_Wk3_Thurs.R
@@ -0,0 +1,14 @@
+#########################################
+### In class exercises - Feb 8, 2022 ####
+#########################################
+library()  ### tidyverse library for reading in data
+library()  ### tidyverse library itself
+
+## Exercise 1 uses the data set that can be loaded below
+wk3Ex_df1 <- read_csv("https://raw.githubusercontent.com/ScienceParkStudyGroup/r-lesson-based-on-ohi-data-training/gh-pages/data/gapminder_wide.csv")
+
+
+
+## Exercise 2: The 'billboard' dataset that comes with TidyR
+tidyr::billboard 
+
